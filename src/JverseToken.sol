f1ec084547472f86c579d4846c39fd32a5515352
jimmy <mac@macdeAir.home>
2023-12-19 23:40:47 +0800

    init erc20

---
+++ src/JverseToken.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.18;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract JverseToken is ERC20("JverseToken", "JT23") {}
